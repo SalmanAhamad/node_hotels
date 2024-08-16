f3cbee2e5f3e4f4ae320a73026acd1edf6a2d8ad
SalmanAhamad <salmanahmad272206@gmail.com>
2024-08-16 14:10:57 +0530

    The warning message indicates that useUnifiedTopology is also deprecated in the latest versions of the MongoDB Node.js driver and should be removed from your connection options.

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const mongoURL = process.env.MONGODB_URL;
 //SETUP MONGODB CONNECTION
 mongoose.connect(mongoURL, {
   // useNewUrlParser: true,  // render is not accepting this code so i comment this code
-  useUnifiedTopology: true,
+  // useUnifiedTopology: true,
 });
 
 //Get the default connection
